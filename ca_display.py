a78b41e5a1fcd981026345853b9b5bad753827b2
Matthew Hartley <mhartley@cantab.net>
2013-07-26 09:06:59 +0100

    Working curses context manager

--- ca_display.py
+++ ca_display.py
@@ -3,6 +3,20 @@
 import curses
 import time
 
+class RawCurses(object):
+
+    def __enter__(self):
+        self.stdscr = curses.initscr()
+        curses.cbreak()
+        curses.noecho()
+
+        return self.stdscr
+
+    def __exit__(self, type, value, traceback):
+        curses.nocbreak()
+        curses.echo()
+        curses.endwin()
+        
 class CARep(object):
 
     def __init__(self, array):
@@ -24,23 +38,18 @@ class CursesDisplay(object):
         self.ca = ca
 
     def display_run(self, n_frames, interval=0.5):
-        stdscr = curses.initscr()
-        curses.cbreak()
-        curses.noecho()
-        value_map = {0: '  ', 1: '*'}
-        for n in range(0, n_frames):
-            #print self.ca.array
-            for (r, row) in enumerate(self.ca.array):
-                str_rep = ''.join(value_map[c] for c in row)
-                stdscr.addstr(r, 0, str_rep)
-                
-            stdscr.refresh()
-            self.ca.update_vote()
-            time.sleep(interval)
+        with RawCurses() as stdscr:
+            value_map = {0: '  ', 1: '*'}
+            for n in range(0, n_frames):
+                #print self.ca.array
+                for (r, row) in enumerate(self.ca.array):
+                    str_rep = ''.join(value_map[c] for c in row)
+                    stdscr.addstr(r, 0, str_rep)
 
-        curses.nocbreak()
-        curses.echo()
-        curses.endwin()
+                stdscr.refresh()
+                self.ca.update_vote()
+                time.sleep(interval)
+
 
         
         
