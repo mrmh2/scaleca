ac2fed2e864dc92ac22bd817304150b3fb255fdc
Matthew Hartley <mhartley@cantab.net>
2013-07-28 15:52:45 +0100

    Updated docs

--- ca_server.py
+++ ca_server.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 
 import zmq
-
-port = "5556"
-
-context = zmq.Context()
-socket = context.socket(zmq.REP)
-socket.bind("tcp://*:%s" % port)
-
 from scaleca.ca_life import CA
 
-test_ca = CA(10, 10)
-test_ca[5,5] = 1
-test_ca[5,4] = 1
-test_ca[5,6] = 1
+def init_server(port):
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind("tcp://*:%s" % port)
+
+    return socket
+
+def main():
+    port = "5556"
+    socket = init_server(port)
+    test_ca = CA(10, 10)
+    test_ca[5,5] = 1
+    test_ca[5,4] = 1
+    test_ca[5,6] = 1
 
 def run_sim(ca):
     ca.update()
