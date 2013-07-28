7db4425b132393a871a7f1f516464779e4ce931b
Matthew Hartley <mhartley@cantab.net>
2013-07-28 16:03:16 +0100

    Tidied up disp_client.py somewhat

--- disp_client.py
+++ disp_client.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
+"""disp_client.py - simple text display client.
+
+Connects to server, then constructs simple update loop which sends a command to
+the server to run a simulation timestep, then displays the results."""
 
 import zmq
 import numpy as np
 
 from scaleca.disp_simple import CADisplay
 
-port = "5556"
+def connect_to_server(port):
+    context = zmq.Context()
+    #print "Connecting to server..."
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://localhost:%s" % port)
 
-context = zmq.Context()
-#print "Connecting to server..."
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:%s" % port)
+    return socket
 
 def string_state_to_array(sstate, shape):
     c_sep = ','.join(sstate)
@@ -32,8 +37,15 @@ def make_update_loop(socket):
 
     return update_loop
 
-ul = make_update_loop(socket)
+def main():
+    port = "5556"
+    socket = connect_to_server(port)
 
-display = CADisplay()
+    ul = make_update_loop(socket)
 
-display.run_display(ul)
+    display = CADisplay()
+
+    display.run_display(ul)
+
+if __name__ == '__main__':
+    main()
