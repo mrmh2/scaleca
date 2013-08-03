37c34388dac3eedb1e14762bfb4e178ec9427493
Matthew Hartley <mhartley@cantab.net>
2013-08-03 22:18:48 +0100

    Updated serve_ca with command line arguments

--- scripts/serve_ca.py
+++ scripts/serve_ca.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import os
 import sys
+import argparse
+import importlib
 
 def setup():
     parent, cdir = os.path.split(os.path.dirname(__file__))
@@ -11,10 +13,26 @@ def setup():
 def main():
     setup()
 
-    from scaleca.cas.ca_life import CA
-    ca = CA(30, 30)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', '-p', help='port on which to start server (default 5556)', 
+                        default='5556')
+    parser.add_argument('--name', '-n', help='name of CA to serve (default ca_vote)',
+                        default='ca_vote')
+    parser.add_argument('--dim', '-d', help='dimension of CA, as RxC where R is rows and C is columns',
+                        default='30x30')
+    args = parser.parse_args()
+
+    port = args.port
+    ca_name = args.name
+    raw_dim = args.dim
+
+    dim = map(int, raw_dim.split('x'))
+
+    ca_modname = 'scaleca.cas.' + ca_name
+    CA = importlib.import_module(ca_modname)
+    ca = CA.CA(*dim)
     ca.fill_random()
-    ca_server = CAServer(ca)
+    ca_server = CAServer(ca, port=port)
     ca_server.serve_forever()
 
 if __name__ == '__main__':
