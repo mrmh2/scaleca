dd900228f39838ddc9f55e267d9acbaf72aba629
Matthew Hartley <mhartley@cantab.net>
2013-08-09 15:21:19 +0100

    State printing and randomised initiation

--- src/cpp/cppca.cpp
+++ src/cpp/cppca.cpp
@@ -1,29 +1,62 @@
 #include<iostream>
+#include<cstdlib>
+
 
 using namespace std;
 
 class CA {
 public:
-  CA(int nrows, int ncols);
-  set(int row, int col);
+  CA(int in_nrows, int ncols); 
+  void set(int row, int col, int value);
   void dump();
-private:
-  int *state_data;
+  void fill_random();
+  int *state_data; // FIXME - this should be private and properly inherited
   int nrows, ncols;
+  int real_nrows, real_ncols;
 };
 
-CA::CA(int in_nrows, int in_ncols)
+
+class CALife: public CA {
+public:
+  CALife(int in_nrows, int in_ncols) : CA(in_nrows, in_ncols)
+  {
+  }
+  void update();
+  //  CALife(int, int);
+};
+
+
+
+void CALife::update()
 {
-  nrows = in_nrows;
+  int r = 5;
+  int c = 5;
+
+  cout << state_data[(1 + c) + (1 + r) * real_ncols] << endl;
+
+}
+
+CA::CA(int in_nrows, int in_ncols) : nrows(in_nrows)
+{
+  //  nrows = in_nrows;
   ncols = in_ncols;
+
+  real_nrows = in_nrows + 2;
+  real_ncols = in_ncols + 2;
+  
   cout << "Constructing " << nrows << "x" << ncols << endl;
-  state_data = (int *) malloc(sizeof(int) * (nrows + 2) * (ncols + 2));
-  memset(state_data, 0, sizeof(int) * (nrows + 2) * (ncols + 2));
+
+  state_data = new int[real_nrows * real_ncols]();
+  //state_data = (int *) malloc(sizeof(int) * (nrows + 2) * (ncols + 2));
+  //  memset(state_data, 0, sizeof(int) * (nrows + 2) * (ncols + 2));
 }
 
 void CA::set(int row, int col, int value)
 {
-  //  state
+  int crow = row + 1;
+  int ccol = col + 1;
+
+  state_data[crow * real_ncols + ccol] = value;
 }
   
 
@@ -35,13 +68,26 @@ void CA::dump()
     }
     cout << endl;
   }
+}
 
+void CA::fill_random()
+{
+  for(int r=0; r<nrows; r++) 
+    for(int c=0; c<ncols; c++)
+      state_data[1 + c + (r + 1) * (real_ncols)] = rand() % 2;
 }
 
 
 int main(int argc, char *argv[])
 {
-  CA ca(10, 10);
+  CALife ca(10, 10);
+  ca.dump();
+  ca.set(5, 5, 1);
+  ca.set(5, 4, 1);
+  ca.set(5, 6, 1);
+  //  ca.fill_random();
+  ca.dump();
+  ca.update();
   ca.dump();
   return 0;
 }
