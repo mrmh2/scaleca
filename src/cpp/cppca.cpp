01e72b9c241bc0d3206622d3402510e8be0603c4
Matthew Hartley <mhartley@cantab.net>
2013-08-13 10:49:31 +0100

    Added timing functions

--- src/cpp/cppca.cpp
+++ src/cpp/cppca.cpp
@@ -1,7 +1,22 @@
 #include<iostream>
 #include<cstdlib>
+#include<ctime>
+#include<sys/time.h>
 
 using namespace std;
+
+double read_timer( )
+{
+  static bool initialized = false;
+  static struct timeval start, end;
+  if( !initialized )
+    {
+      gettimeofday( &start, NULL );
+      initialized = true;
+    }
+  gettimeofday( &end, NULL );
+  return (end.tv_sec - start.tv_sec) + 1.0e-6 * (end.tv_usec - start.tv_usec);
+}
 
 class CA {
 public:
@@ -257,9 +272,28 @@ void test_vote()
   }
 }
 
+void test_timing(){
+  CAVote ca(1000, 1000);
+  ca.fill_random();
+
+  int gen_count = 0;
+  double start = read_timer();
+  while (1) {
+    ca.update();
+    gen_count++;
+    if (gen_count%50 == 0) {
+      double gen_time = (read_timer() - start) / (double) gen_count;
+      cout << gen_time << "ms per generation." << endl;
+    }
+  }
+}
+    
+    
+
 int main(int argc, char *argv[])
 {
   //  test_glider();
-  test_vote();
+  //  test_vote();
+  test_timing();
   return 0;
 }
